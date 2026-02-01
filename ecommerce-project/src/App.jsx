75582c45b0b7092225c2b7469b199473a700f363
ANSHUL5911 <the.anshul.sj@gmail.com>
2026-02-01 22:24:10 +0530

    Refactor index.html and App.jsx; remove unused checkout files

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from 'react-router';
-import './App.css'
-import { HomePage } from './pages/HomePage'
+import './App.css';
+import { HomePage } from './pages/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage';
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='checkout' element={<div>Checkout</div>} />
+      <Route path='checkout' element={<CheckoutPage />} />
     </Routes>
-    
+
   )
 }
 
