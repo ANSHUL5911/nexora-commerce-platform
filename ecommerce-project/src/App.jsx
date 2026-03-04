8e6cbda57cea03285e6f5f6c416c094cebb20e6d
ANSHUL5911 <the.anshul.sj@gmail.com>
2026-03-04 20:55:02 +0530

    Separate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { useState, useEffect } from 'react';
 import './App.css';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 
 
@@ -20,9 +20,9 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage cart={ cart } />} />
-      <Route path='checkout' element={<CheckoutPage cart={ cart } />} />
-      <Route path="orders" element={<OrdersPage cart={ cart } />} />
+      <Route path='/' element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
 
